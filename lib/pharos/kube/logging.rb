f76d69f44e918857ab7f3ee790815ddb7cdbc0b4
Tero Marttila <tero.marttila@kontena.io>
2018-07-11 11:54:14 +0300

    per-class log level

--- lib/pharos/kube/logging.rb
+++ lib/pharos/kube/logging.rb
@@ -6,22 +6,26 @@ module Pharos
       LOG_TARGET = STDERR
       LOG_LEVEL = Logger::WARN
 
-      # global log_level shared across all including classes
-      def self.log_level
-        @log_level ||= LOG_LEVEL
+      module ModuleMethods
+        # global log_level shared across all including classes
+        def log_level
+          @log_level
+        end
+        def log_level=(level)
+          @log_level = level
+        end
+        def debug!
+          self.log_level = Logger::DEBUG
+        end
+        def verbose!
+          self.log_level = Logger::INFO
+        end
+        def quiet!
+          self.log_level = Logger::ERROR
+        end
       end
-      def self.log_level=(level)
-        @log_level = level
-      end
-      def self.debug!
-        self.log_level = Logger::DEBUG
-      end
-      def self.verbose!
-        self.log_level = Logger::INFO
-      end
-      def self.quiet!
-        self.log_level = Logger::ERROR
-      end
+
+      extend ModuleMethods # global @log_level
 
       module ClassMethods
         def logger(target: LOG_TARGET, level: Pharos::Kube::Logging.log_level)
@@ -33,14 +37,17 @@ module Pharos
       end
 
       def self.included(base)
+        base.extend(ModuleMethods) # per-class @log_level
         base.extend(ClassMethods)
       end
 
       # Use per-instance logger
-      def logger!(progname: self.class.name, target: LOG_TARGET, level: Pharos::Kube::Logging.log_level, debug: false)
+      def logger!(progname: self.class.name, target: LOG_TARGET, level: nil, debug: false)
         @logger = Logger.new(target).tap do |logger|
+          level = Logger::DEBUG if debug
+
           logger.progname = "#{self.class.name}<#{progname}>"
-          logger.level = debug ? Logger::DEBUG : level
+          logger.level = level || self.class.log_level || Pharos::Kube::Logging.log_level || LOG_LEVEL
         end
       end
 
