174f097adafd11938aa214d1251bca4be107e4e3
Tero Marttila <tero.marttila@kontena.io>
2018-08-21 11:18:41 +0300

    spec basic stack apply

--- spec/pharos/kube/stack_spec.rb
+++ spec/pharos/kube/stack_spec.rb
@@ -40,7 +40,27 @@ RSpec.describe K8s::Stack do
     end
 
     context "which is not yet installed" do
+      let(:resources) {
+        subject.resources.map{ |r| r.merge(
+          metadata: {
+            labels: { 'k8s.kontena.io/stack' => 'whoami' },
+            annotations: { 'k8s.kontena.io/stack-checksum' => subject.checksum },
+          },
+        ) }
+      }
 
+      before do
+        allow(client).to receive(:get_resources).with([K8s::Resource, K8s::Resource, K8s::Resource]).and_return([nil, nil, nil ])
+        allow(client).to receive(:list_resources).with(labelSelector: { 'k8s.kontena.io/stack' => 'whoami' }).and_return(resources)
+      end
+
+      it "creates the resource with the correct label" do
+        resources.each do |r|
+          expect(client).to receive(:create_resource).with(r).and_return(r)
+        end
+
+        subject.apply(client)
+      end
     end
   end
 end
