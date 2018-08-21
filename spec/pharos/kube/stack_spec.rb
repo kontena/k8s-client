50cff56b8b5d81507cd2efc2d6195cfd7d5b364b
Tero Marttila <tero.marttila@kontena.io>
2018-08-21 11:13:48 +0300

    fix stack specs

--- spec/pharos/kube/stack_spec.rb
+++ spec/pharos/kube/stack_spec.rb
@@ -4,10 +4,43 @@ RSpec.describe K8s::Stack do
   context "for a simple whoami stack" do
     let(:client) { instance_double(K8s::Client) }
 
-    subject { described_class.load(fixture_path('stacks/whoami.yaml'), name: 'whoami') }
+    subject { described_class.load('whoami', fixture_path('stacks/whoami.yaml')) }
+
+    it "has the correct name" do
+      expect(subject.name).to eq 'whoami'
+    end
+
+    it "has the resource" do
+      expect(subject.resources.map{|r| r.to_hash}).to match [
+        hash_including(
+          apiVersion: 'apps/v1',
+          kind: 'Deployment',
+          metadata: hash_including(
+            namespace: 'default',
+            name: 'whoami',
+          ),
+        ),
+        hash_including(
+          apiVersion: 'v1',
+          kind: 'Service',
+          metadata: hash_including(
+            namespace: 'default',
+            name: 'whoami',
+          ),
+        ),
+        hash_including(
+          apiVersion: 'extensions/v1beta1',
+          kind: 'Ingress',
+          metadata: hash_including(
+            namespace: 'default',
+            name: 'whoami',
+          ),
+        ),
+      ]
+    end
 
     context "which is not yet installed" do
-      
+
     end
   end
 end
