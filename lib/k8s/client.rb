0c1a366b603cc7096a8e2201efc29bf196b867c7
Kimmo Lehto <kimmo.lehto@kontena.io>
2018-10-15 14:24:39 +0300

    Ignore Naming/FileName for lib/k8s-client.rb only

--- lib/k8s/client.rb
+++ lib/k8s/client.rb
@@ -3,6 +3,20 @@
 require 'openssl'
 require 'base64'
 
+require 'k8s/api/metav1'
+require 'k8s/api/version'
+
+require 'k8s/config'
+require 'k8s/logging'
+
+require 'k8s/api_client'
+require "k8s/error"
+require 'k8s/resource'
+require 'k8s/resource_client'
+require 'k8s/stack'
+require 'k8s/transport'
+require 'k8s/util'
+
 module K8s
   # @param server [String] http/s URL
   # @param options [Hash] @see Transport.new
